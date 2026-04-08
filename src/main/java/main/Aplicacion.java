8cac8e2672b810b143aea5e5f04385d1fdc27bbc
Rosa Maria Peña <rosamariapca10@gmail.com>
2026-04-08 10:00:52 -0500

    Correccion de estructura del ciclo for y aplicación correcta de polimorfismo

--- src/main/java/main/Aplicacion.java
+++ src/main/java/main/Aplicacion.java
@@ -1,23 +1,16 @@
 package main;
 
-/**
- *
- * @author rosam
- */
-
 import controlador.ControladorCurso;
 import vista.VistaCurso;
 
 public class Aplicacion {
     public static void main(String[] args) {
-        
-       
+
         VistaCurso vista = new VistaCurso();
-
         int cantidad = vista.leerCantidad();
-
         ControladorCurso controlador = new ControladorCurso(cantidad);
-        
+
+        // solo para agregar estudiantes
         for (int i = 0; i < cantidad; i++) {
 
             int codigo = vista.leerCodigo();
@@ -25,10 +18,15 @@ public class Aplicacion {
 
             double software = vista.leerNota("Software");
             double hardware = vista.leerNota("Hardware");
-            double matematica = vista.leerNota("Matematica"); 
-            
-            controlador.agregar(i, codigo, nombre, software, hardware, matematica);
-        }
+            double matematica = vista.leerNota("Matematica");
+
+            // POLIMORFISMO
+            if (i % 2 == 0) {
+                controlador.agregar(i, codigo, nombre, software, hardware, matematica);
+            } else {
+                controlador.agregar(i, codigo, nombre, software, hardware, matematica, 0.3);
+            }
+        } 
 
         System.out.println("Lista ordenada:");
         controlador.getCurso().ordenarAscendente();
@@ -46,7 +44,7 @@ public class Aplicacion {
         vista.mostrar(controlador.getCurso().getEstudiantes(), cantidad);
 
         System.out.println("Modificar notas del estudiante:");
-        int codigo = vista.leerCodigo();       
+        int codigo = vista.leerCodigo();
         double nuevoSoftware = vista.leerNota("Nueva nota de software");
         double nuevoHardware = vista.leerNota("Nueva nota de Hardware");
         double nuevaMatematica = vista.leerNota("Nueva nota de matematicas");
@@ -58,4 +56,3 @@ public class Aplicacion {
         vista.mostrar(controlador.getCurso().getEstudiantes(), cantidad);
     }
 }
-
